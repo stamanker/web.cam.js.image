c1ac3460928cb60a86446370d9d8350a98fe5cf8
maxz <maxim.zemlyanoy@gmail.com>
2019-06-13 01:52:34 +0300

    yet more tiny improvement

--- src/main/java/ua/od/stamanker/web/webcam/DateUtils.java
+++ src/main/java/ua/od/stamanker/web/webcam/DateUtils.java
@@ -5,8 +5,8 @@ import java.util.Date;
 
 public class DateUtils {
 
-    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd_HH-mm-ss");
-    public static final SimpleDateFormat DATE_FORMAT_DAY_HOUR = new SimpleDateFormat("dd_HH");
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+    public static final SimpleDateFormat DATE_FORMAT_DAY_HOUR = new SimpleDateFormat("yyyy-MM-dd__HH");
 
     public static String getCurrentHour() {
         return DATE_FORMAT_DAY_HOUR.format(new Date());
