99675a63cbb27de75ef22543b2c88a843f1e61eb
maxz <maxim.zemlyanoy@gmail.com>
2020-07-03 21:38:14 +0300

    Added 'DesktopApp', 'PanelExample'

---
+++ src/main/java/ua/od/stamanker/web/webcam/DesktopApp.java
@@ -0,0 +1,62 @@
+package ua.od.stamanker.web.webcam;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.channels.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class DesktopApp {
+
+    WebCamManager webCamManager;
+
+    private DesktopApp init() {
+        webCamManager = WebCamManager.getInstance();
+        return this;
+    }
+
+    public DesktopApp process() {
+        AtomicReference<byte[]> image = webCamManager.image;
+        if (image.get() == null) {
+            System.out.println("No data");
+            return this;
+        }
+        Socket socket = new Socket();
+        try {
+            socket.connect(new InetSocketAddress("", 8081));
+            socket.setTcpNoDelay(true);
+            try (OutputStream outputStream = socket.getOutputStream()) {
+                outputStream.write(image.get());
+            }
+            socket.close();
+            System.out.println("Send");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket.isConnected()) {
+                try {
+                    socket.close();
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        return this;
+    }
+
+    private AsynchronousServerSocketChannel openConnection() {
+        try {
+            return AsynchronousServerSocketChannel.open();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        DesktopApp desktopApp = new DesktopApp().init();
+        while (true) {
+            desktopApp.process();
+            Thread.sleep(2_000);
+        }
+    }
+}
---
+++ src/main/java/ua/od/stamanker/web/webcam/PanelExample.java
@@ -0,0 +1,46 @@
+package ua.od.stamanker.web.webcam;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+public class PanelExample {
+
+        public static void main(String[] args) {
+
+            ArrayList l = new ArrayList();
+            l.remove(1);
+
+            final Webcam webcam = Webcam.getDefault();
+            webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+            final WebcamPanel panel = new WebcamPanel(webcam);
+            panel.setFPSDisplayed(true);
+            panel.setImageSizeDisplayed(true);
+            panel.setMirrored(true); //rotation: https://github.com/sarxos/webcam-capture/issues/581
+            panel.setDrawMode(WebcamPanel.DrawMode.FILL);
+
+            final JCheckBox flip = new JCheckBox();
+            flip.setSelected(true);
+            flip.setAction(new AbstractAction("Flip") {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    panel.setMirrored(flip.isSelected());
+                }
+            });
+
+            JFrame window = new JFrame("Test webcam panel");
+            window.setLayout(new FlowLayout());
+            window.add(panel);
+            window.add(flip);
+            window.setResizable(true);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.pack();
+            window.setVisible(true);
+        }
+}
